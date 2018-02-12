5947839387c107b7acd57d02e3302e50d7ede8ba
Irevall <kubajmarek@gmail.com>
2018-02-12 21:05:53 +0100

    Adds saving to localStorage

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## Next on the "to do" list of to-do project:
-0. Clean up to-do-creator code
 1. Only one element can be edited at given time (sort of done with 'blur' listener, but needs more work)
 2. Add non-basic styling 
 3. Clean up code (XD)
--- toDoList.js
+++ toDoList.js
@@ -1,39 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     let allToDoElements = [];
-    
-    
-    let toDoCreator = document.querySelector('.to-do-creator');
 
+    function retrievingList() {
+        if (localStorage.length !== 0) {
+            if (confirm('Do you want to retrieve previous To Do List?')) {
+                localStorage.previousList.split(',').forEach((element) => {
+                    addAnother(element);
+                });
+            } else {
+                localStorage.removeItem('previousList');
+            }
+        }
+    }
 
-    toDoCreator.querySelector('input').addEventListener('keyup', (e) => {
-        if (event.keyCode === 13) {
-            if (toDoCreator.querySelector('.creator-input').value.length===0) {
+    function addCreatorListeners() {
+        let toDoCreator = document.querySelector('.to-do-creator');
+
+        toDoCreator.querySelector('input').addEventListener('keyup', (e) => {
+            if (e.keyCode === 13) {
+                if (toDoCreator.querySelector('.creator-input').value.length === 0) {
+                    alert('Can\'t add empty element');
+                    return false;
+                }
+                addAnother(toDoCreator.querySelector('.creator-input').value);
+                toDoCreator.querySelector('.creator-input').value = '';
+            }
+        });
+
+        toDoCreator.querySelector('.add-button').addEventListener('click', (e) => {
+            if (toDoCreator.querySelector('.creator-input').value.length === 0) {
                 alert('Can\'t add empty element');
                 return false;
             }
-            addAnother();
-        }
-    });
+            addAnother(toDoCreator.querySelector('.creator-input').value);
+            toDoCreator.querySelector('.creator-input').value = '';
+        });
 
-    toDoCreator.querySelector('.add-button').addEventListener('click', (e) => {
-        if (toDoCreator.querySelector('.creator-input').value.length===0) {
-            alert('Can\'t add empty element');
-            return false;
-        }
-        addAnother();
-    });
+        toDoCreator.addEventListener('drop', (e) => {
+            e.preventDefault();
+        });
+    }
 
-    toDoCreator.addEventListener('drop', (e) => {
-        e.preventDefault();
-    });
+    function saveList() {
+        let arrayOfContents = [];
+        allToDoElements.forEach((element) => {
+           arrayOfContents.push(element.content);
+        });
+        localStorage.setItem('previousList', arrayOfContents);
+    }
 
-    function addAnother() {
+    function addAnother(arg) {
         const divPrototype = document.querySelector("#temp").content.cloneNode(true);
         document.querySelector('#to-do-list').appendChild(divPrototype);
-        allToDoElements.push(new toDoElement(document.querySelector('.to-do-creator').querySelector('.creator-input').value));
-        console.log(allToDoElements);
+        allToDoElements.push(new toDoElement(arg));
         allToDoElements.slice(-1).pop().addNew();
+        saveList();
     }
 
     function swapNodes(node1, node2) {
@@ -48,38 +70,60 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    function toDoElement(arg) {
-        this.content = arg;
-        this.index = allToDoElements.length + 1;
-        this.element = document.querySelector('.to-do-element:last-of-type');
-        this.addNew();
-        this.addListeners();
-    }
+    class toDoElement {
+        constructor(arg) {
+            this.content = arg;
+            this.index = allToDoElements.length + 1;
+            this.element = document.querySelector('.to-do-element:last-of-type');
+            this.addNew();
+            this.addListeners();
+        }
 
-    toDoElement.prototype = {
         addNew() {
                 this.element.querySelector('.index').innerText = this.index;
                 this.element.querySelector('.to-do-text').innerText = this.content;
                 this.element.querySelector('.to-do-input').value = this.content;
-                toDoCreator.querySelector('.creator-input').value = '';
-        },
+        }
+
         addListeners() {
-            console.log(this);
+            this.addClickListeners();
+            this.addDragListeners();
+        }
+
+        addClickListeners() {
             this.element.querySelector('input').addEventListener('keyup', (e) => {
                 if (e.keyCode === 13) {
                     this.save();
                 }
             });
 
-            this.element.querySelector('.text-wrapper').addEventListener('dblclick', () => {this.edit()});
-            this.element.querySelector('.save-button').addEventListener('click', () => {this.save()});
-            this.element.querySelector('.edit-button').addEventListener('click', () => {this.edit()});
-            this.element.querySelector('.delete-button').addEventListener('click', () => {this.deleteIt()});
-            this.element.querySelector('.cross-out-button').addEventListener('click', () => {this.crossOut()});
-            this.element.querySelector('.uncross-out-button').addEventListener('click', () => {this.uncrossOut()});
-            this.element.querySelector('.move-up-button').addEventListener('click', () => {this.moveUp()});
-            this.element.querySelector('.move-down-button').addEventListener('click', () => {this.moveDown()});
+            this.element.querySelector('.text-wrapper').addEventListener('dblclick', () => {
+                this.edit();
+            });
+            this.element.querySelector('.save-button').addEventListener('click', () => {
+                this.save();
+            });
+            this.element.querySelector('.edit-button').addEventListener('click', () => {
+                this.edit();
+            });
+            this.element.querySelector('.delete-button').addEventListener('click', () => {
+                this.delete();
+            });
+            this.element.querySelector('.cross-out-button').addEventListener('click', () => {
+                this.crossOut();
+            });
+            this.element.querySelector('.uncross-out-button').addEventListener('click', () => {
+                this.uncrossOut();
+            });
+            this.element.querySelector('.move-up-button').addEventListener('click', () => {
+                this.moveUp();
+            });
+            this.element.querySelector('.move-down-button').addEventListener('click', () => {
+                this.moveDown();
+            });
+        }
 
+        addDragListeners() {
             this.element.addEventListener('dragover', (e) => {
                 e.preventDefault();
             });
@@ -102,64 +146,78 @@ document.addEventListener('DOMContentLoaded', function() {
                     allToDoElements.splice(index1 - 1 , 1);
                 }
                 fixingIndex();
+                saveList();
             });
-        },
+        }
+
         save() {
-            if (selectors.call(this).toDoInput.value.length===0) {
+            if (this.selectors().toDoInput.value.length===0) {
                 alert('To Do element cannot be empty.');
                 return false;
             }
             this.element.classList.remove("editing");
             this.element.classList.add("saved");
-            this.content = selectors.call(this).toDoInput.value;
-            selectors.call(this).toDoText.innerText = this.content;
-        },
+            this.content = this.selectors().toDoInput.value;
+            this.selectors().toDoText.innerText = this.content;
+            saveList();
+        }
+
         edit() {
             this.element.classList.remove("saved");
             this.element.classList.add("editing");
-            selectors.call(this).toDoInput.addEventListener('blur', () => {
+            this.selectors().toDoInput.addEventListener('blur', () => {
                 this.save();
             });
-        },
-        deleteIt() {
+        }
+
+        delete() {
             this.element.remove();
-            allToDoElements.splice(this.index - 1, 1)
-            console.log(allToDoElements);
+            allToDoElements.splice(this.index - 1, 1);
             fixingIndex();
-        },
+            saveList();
+        }
+
         crossOut() {
             this.element.classList.add("crossed-out");
-        },
+        }
+
         uncrossOut() {
             this.element.classList.remove("crossed-out");
-        },
+        }
+
         moveUp() {
             swapNodes(allToDoElements[this.index-2].element, this.element);
             allToDoElements.splice(this.index-2, 0, this);
-            allToDoElements.splice(this.index);
+            allToDoElements.splice(this.index, 1);
             fixingIndex();
-        },
+            saveList();
+        }
+
         moveDown() {
             swapNodes(this.element, allToDoElements[this.index].element);
             allToDoElements.splice(this.index-1, 0, allToDoElements[this.index]);
             allToDoElements.splice(this.index+1, 1);
             fixingIndex();
-        },
-    }
+            saveList();
+        }
 
-    function selectors() {
-        return {
-            index: this.element.querySelector('.index').innerText,
-            toDoText: this.element.querySelector('.to-do-text'),
-            toDoInput: this.element.querySelector('.to-do-input'),
-            saveButton: this.element.querySelector('.save-button'),
-            editButton: this.element.querySelector('.edit-button'),
-            deleteButton: this.element.querySelector('.delete-button'),
-            crossoutButton: this.element.querySelector('.cross-out-button'),
-            uncrossoutButton: this.element.querySelector('.uncross-out-button'),
-            moveUpButton: this.element.querySelector('.move-up-button'),
-            moveDownButton: this.element.querySelector('.move-down-button')
+        selectors() {
+            return {
+                index: this.element.querySelector('.index').innerText,
+                toDoText: this.element.querySelector('.to-do-text'),
+                toDoInput: this.element.querySelector('.to-do-input'),
+                saveButton: this.element.querySelector('.save-button'),
+                editButton: this.element.querySelector('.edit-button'),
+                deleteButton: this.element.querySelector('.delete-button'),
+                crossoutButton: this.element.querySelector('.cross-out-button'),
+                uncrossoutButton: this.element.querySelector('.uncross-out-button'),
+                moveUpButton: this.element.querySelector('.move-up-button'),
+                moveDownButton: this.element.querySelector('.move-down-button')
+            }
         }
     }
+
+    addCreatorListeners();
+    retrievingList();
 });
 
